58cd4be26b8d512d715f0971753a4dea5de33b9c
Jude Froehlich <chester.t.ferret@gmail.com>
2026-09-02 14:57:15 -0500

    Enhance CPU decision-making and player prompts
    Refactor CPU choice logic for tic-tac-toe game and improve player interaction.

--- main.cpp
+++ main.cpp
@@ -7,7 +7,9 @@
 
 
 
-
+char cpu_char = 'O';
+char your_char = 'X';
+int moves_made = 0;
 
 void print_board(char board[9]) {
   for (int i = 0; i < 9; i++) {
@@ -30,6 +32,110 @@ int get_random_choice(char board[9]) {
   return b[index];   // return the chosen index
 }
 
+int get_cpu_choice(char board[9]) {
+  /* We want to first win, then block, then go middle if applicable, 
+  then connect into two, then go corner, then random */
+  // Wins
+  // Put diagonals FIRST because they are the best wins (usually)
+  std::array<std::array<int, 3>, 8> wins = {{
+      {0,4,8}, {2,4,6},            // diagonals
+      {0,1,2}, {3,4,5}, {6,7,8},   // rows
+      {0,3,6}, {1,4,7}, {2,5,8}    // columns
+      
+  }};
+
+  // corners
+  std::array<int, 4> corners = {0, 2, 6, 8};
+  // Get all possible moves
+  std::vector<int> b;
+  for (int i = 0; i < 9; i++) {
+    if (board[i] != 'X' && board[i] != 'O') {
+      b.push_back(i);   // push index
+    }
+  }
+
+  // Check for wins (order of the 2d arr matters here)
+  for (auto line : wins) {
+    std::vector<int> os;
+    for (int i = 0; i < 3; i++) {
+      if (board[line[i]] == cpu_char) {
+        os.push_back(board[line[i]]);
+      }
+      
+    }
+    // Check for a 3rd O for the win
+    if (os.size() == 2) {
+      for (int i = 0; i < 3; i++) {
+        if (board[line[i]] == cpu_char) {
+          continue;
+        } else if (board[line[i]] != your_char) {
+          return line[i];
+        }
+      }
+    }
+
+  }
+
+  // Check for blocks
+  for (auto line : wins) {
+    std::vector<int> xs;
+    for (int i = 0; i < 3; i++) {
+      if (board[line[i]] == your_char)
+      {
+        xs.push_back(board[line[i]]);
+      }
+      
+    }
+   // Check for a 3rd X for a block
+    if (xs.size() == 2) {
+      for (int i = 0; i < 3; i++) {
+        if (board[line[i]] == your_char) {
+          continue;
+        } else if (board[line[i]] != cpu_char) {
+          return line[i];
+        }
+      }
+    }
+
+  }
+
+  // Go middle if applicable
+  if (board[5] == '5') {
+    return 5;
+  }
+
+  // try to connect one into two
+  for (auto line : wins) {
+    std::vector<int> os;
+    for (int i = 0; i < 3; i++) {
+      if (board[line[i]] == cpu_char) {
+        os.push_back(board[line[i]]);
+      }
+      
+    }
+    // Check for a 3rd O for the win
+    if (os.size() == 1) {
+      for (int i = 0; i < 3; i++) {
+        if (board[line[i]] == cpu_char) {
+          continue;
+        } else if (board[line[i]] != your_char) {
+          return line[i];
+        }
+      }
+    }
+  }
+
+  // Check corners
+  for (int corner : corners) {
+    if (board[corner] != your_char && board[corner] != cpu_char) {
+      return corner;
+    }
+  }
+
+  return get_random_choice(board);
+
+}
+
 
 std::optional<bool> check_winner(char board[9]) {
     std::array<std::array<int, 3>, 8> wins = {{
@@ -41,9 +147,9 @@ std::optional<bool> check_winner(char board[9]) {
     for (auto line : wins) {
         if (board[line[0]] == board[line[1]] &&
             board[line[1]] == board[line[2]]) {
-            if (board[line[0]] == 'X') {
+            if (board[line[0]] == your_char) {
                 return true;   // Player wins
-            } else if (board[line[0]] == 'O') {
+            } else if (board[line[0]] == cpu_char) {
                 return false;  // CPU wins
             }
         }
@@ -60,6 +166,25 @@ int main() {
 
   char sq;
   bool found;
+
+  bool first = true;
+  std::string turn = "";
+  while (turn != "y" && turn != "Y" && turn != "n" && turn != "N") {
+    std::cout << "Do you want to go first? (y/n) ";
+    std::cin >> turn; 
+  }
+
+  if (turn == "n") {
+    std::cout << "You are O's\n";
+    cpu_char = 'X';
+    your_char = 'O';
+    int ind = get_cpu_choice(board); // cpu
+    board[ind] = cpu_char;
+    print_board(board);
+  } else {
+    std::cout << "You are X's\n";
+  }
+
   while (true) {
 
     while (!found) {
@@ -69,7 +194,8 @@ int main() {
       for (int i = 0; i < sizeof(board)/sizeof(board[0]); i++) {
           if (board[i] == sq) {
               found = true;
-              board[i] = 'X';
+              board[i] = your_char;
+              moves_made++;
               break;
           }
       }
@@ -89,8 +215,15 @@ int main() {
       return 0;
     }
 
-    int ind = get_random_choice(board);
-    board[ind] = 'O';
+    if (moves_made == 9) {
+       std::cout << "It's a tie!";
+       return 0;
+    }
+
+    // int ind = get_random_choice(board); // gets random choice
+    int ind = get_cpu_choice(board); // cpu
+    board[ind] = cpu_char;
+    moves_made++;
 
     print_board(board);
 
@@ -108,8 +241,13 @@ int main() {
 
     found = false;
 
+    if (moves_made == 9) {
+       std::cout << "It's a tie!";
+       return 0;
+    }
+
   }
+
 
   return 0;
 }
-
